bf31394c0db277ca9020a2aca8121e42195ec421
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-12-07 11:03:34 +0900

    add test for TasksFragment

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragmentTest.kt
@@ -0,0 +1,66 @@
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import android.os.Bundle
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.ServiceLocator
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeAndroidTestRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import com.example.android.architecture.blueprints.todoapp.taskdetail.TaskDetailFragmentDirections
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+@ExperimentalCoroutinesApi
+class TasksFragmentTest {
+    private lateinit var repository: TasksRepository
+
+    @Before
+    fun initRepository() {
+        repository = FakeAndroidTestRepository()
+        ServiceLocator.tasksRepository = repository
+    }
+
+    @After
+    fun cleanupDb() = runBlockingTest {
+        ServiceLocator.resetRepository()
+    }
+
+    @Test
+    fun clickTask_navigateToDetailFragmentOne() = runBlockingTest {
+        repository.saveTask(Task("TITLE1", "DESCRIPTION1", false, "id1"))
+        repository.saveTask(Task("TITLE2", "DESCRIPTION2", true, "id2"))
+
+        // GIVEN - on the home screen
+        val scenario = launchFragmentInContainer<TasksFragment>(Bundle(), R.style.AppTheme)
+        val navController = mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        // WHEN - click on the first item
+        onView(withId(R.id.tasks_list)).perform(
+                RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
+                        hasDescendant(withText("TITLE1")),
+                        click()
+                )
+        )
+
+        // THEN - verify that we navigate to correct item's detail screen
+        verify(navController).navigate(
+                TasksFragmentDirections.actionTasksFragmentToTaskDetailFragment("id1")
+        )
+    }
+}
