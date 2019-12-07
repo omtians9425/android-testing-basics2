7c3c3de0ca2316a2db824c2d84bf2d4193a9d0eb
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-12-07 10:28:59 +0900

    implement taskDetailFragmentTest

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -1,24 +1,58 @@
 package com.example.android.architecture.blueprints.todoapp.taskdetail
 
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.ServiceLocator
 import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeAndroidTestRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@ExperimentalCoroutinesApi
 @MediumTest
 @RunWith(AndroidJUnit4::class)
 class TaskDetailFragmentTest {
 
+    private lateinit var tasksRepository: TasksRepository
+
+    @Before
+    fun initRepository() {
+        tasksRepository = FakeAndroidTestRepository()
+        ServiceLocator.tasksRepository = tasksRepository
+    }
+
+    @After
+    fun cleanupDb() = runBlockingTest {
+        ServiceLocator.resetRepository()
+    }
+
     @Test
-    fun activeTaskDetails_DisplayedInUi() {
+    fun activeTaskDetails_DisplayedInUi() = runBlockingTest{ // Do not execute in parallel
         // GIVEN active task
         val activeTask = Task("title1", "AndroidX Rocks", false)
+        tasksRepository.saveTask(activeTask)
 
         // WHEN - Details Fragment launched to display task
         val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
         launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+
+        // THEN - tasks details are displayed in the screen
+        onView(withId(R.id.task_detail_title_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_title_text)).check(matches(withText("title1")))
+        onView(withId(R.id.task_detail_description_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_description_text)).check(matches(withText("AndroidX Rocks")))
+        //and: task status checkbox is displayed and unchecked
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isNotChecked()))
     }
 }
